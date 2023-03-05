1c492545b6ac5483edeea0eabd0a22db2358d0c1
nairaditya99 <92709456+nairaditya99@users.noreply.github.com>
2023-03-05 17:36:34 +0530

    Add files via upload

---
+++ MentalHealthBot/app.py
@@ -0,0 +1,70 @@
+import streamlit as st
+from streamlit_chat import message
+from gpt_index import SimpleDirectoryReader, GPTListIndex, readers, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+from langchain import OpenAI
+import sys
+import os
+
+def construct_index(directory_path):
+    # set maximum input size
+    max_input_size = 4096
+    # set number of output tokens
+    num_outputs = 300
+    # set maximum chunk overlap
+    max_chunk_overlap = 20
+    # set chunk size limit
+    chunk_size_limit = 600 
+
+    # define LLM
+    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.5, model_name="text-davinci-003", max_tokens=num_outputs))
+    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+ 
+    documents = SimpleDirectoryReader(directory_path).load_data()
+    
+    index = GPTSimpleVectorIndex(
+        documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper
+    )
+
+    index.save_to_disk('index.json')
+
+    return index
+
+
+
+st.title("AURA🤖: An AI friend to help you with your mental health journey ")
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+def get_text():
+    input_text = st.text_input("You: ","Hello, how are you?", key="input")
+    return input_text 
+
+user_input = get_text()
+
+def ask_ai(user_input):
+    index = GPTSimpleVectorIndex.load_from_disk('index.json')
+    input = input
+    response = index.query(user_input, response_mode='compact')
+    return response
+
+os.environ["OPENAI_API_KEY"] = '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'
+
+construct_index("data")
+
+
+user_input = get_text()
+
+if user_input:
+    output = ask_ai(user_input)
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+
+if st.session_state['generated']:
+
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
